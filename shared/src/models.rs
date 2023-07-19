764fb6aa474301498b05bae6fecd034fb30d6a56
petrostrak <pit.trak@gmail.com>
2023-07-19 15:53:43 +0300

    add models

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,3 +1,13 @@
+pub struct Film {
+    pub id: uuid::Uuid, // we will be using uuids as ids
+    pub title: String,
+    pub director: String,
+    pub year: u16,
+    pub poster: String,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
 pub struct CreateFilm {
     pub title: String,
     pub director: String,
