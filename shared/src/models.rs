ca4c74fc8b1eef110723d77fe330a17cd41fdcce
petrostrak <pit.trak@gmail.com>
2023-07-20 09:24:59 +0300

    remove comment

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Film {
-    pub id: uuid::Uuid, // we will be using uuids as ids
+    pub id: uuid::Uuid,
     pub title: String,
     pub director: String,
     pub year: u16,
