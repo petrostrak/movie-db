7a62cd32115785b304b3b148d7bef7e4f11bfbbf
petrostrak <pit.trak@gmail.com>
2023-07-21 13:41:14 +0300

    serve static files

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::web::ServiceConfig;
+use std::path::PathBuf;
+
+use actix_web::web::{self, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
@@ -6,6 +8,7 @@ use sqlx::Executor;
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     // initialize the database if not already initialized
     pool.execute(include_str!("../../db/schema.sql"))
@@ -16,10 +19,20 @@ async fn actix_web(
     let film_repo = actix_web::web::Data::new(film_repo);
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(film_repo)
-            .configure(api_lib::health::service)
-            .configure(api_lib::version::service)
-            .configure(api_lib::films::service::<api_lib::film_repository::PostgresFilmRepository>);
+        cfg.service(
+            web::scope("/api")
+                .app_data(film_repo)
+                .configure(api_lib::health::service)
+                .configure(api_lib::version::service)
+                .configure(
+                    api_lib::films::service::<api_lib::film_repository::PostgresFilmRepository>,
+                ),
+        )
+        .service(
+            actix_files::Files::new("/", static_folder)
+                .show_files_listing()
+                .index_file("index.html"),
+        );
     };
 
     Ok(config.into())
