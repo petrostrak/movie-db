2d4b5a22e79d904be4c8d1325cc9314297ff0bd8
petrostrak <pit.trak@gmail.com>
2023-07-23 12:26:00 +0300

    Add comments in front/main

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,19 +1,40 @@
 #![allow(non_snake_case)]
-use dioxus::prelude::*;
-
 mod components;
 mod models;
+
 use components::{FilmCard, FilmModal, Footer, Header};
+use dioxus::prelude::*;
 use models::FilmModalVisibility;
 use shared::models::Film;
 
 const API_ENDPOINT: &str = "api/v1";
 
+fn films_endpoint() -> String {
+    let window = web_sys::window().expect("no global `window` exists");
+    let location = window.location();
+    let host = location.host().expect("should have a host");
+    let protocol = location.protocol().expect("should have a protocol");
+    let endpoint = format!("{}//{}/{}", protocol, host, API_ENDPOINT);
+    format!("{}/films", endpoint)
+}
+
+async fn get_films() -> Vec<Film> {
+    log::info!("Getting films {}", films_endpoint());
+    reqwest::get(&films_endpoint())
+        .await
+        .unwrap()
+        .json::<Vec<Film>>()
+        .await
+        .unwrap()
+}
+
 fn main() {
     wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
+    // launch the web app
     dioxus_web::launch(App);
 }
 
+// create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     use_shared_state_provider(cx, || FilmModalVisibility(false));
     let is_modal_visible = use_shared_state::<FilmModalVisibility>(cx).unwrap();
@@ -23,15 +44,14 @@ fn App(cx: Scope) -> Element {
 
     {
         let films = films.clone();
-
         use_effect(cx, force_get_films, |_| async move {
             let existing_films = get_films().await;
             if existing_films.is_empty() {
-                films.set(None)
+                films.set(None);
             } else {
                 films.set(Some(existing_films));
             }
-        })
+        });
     }
 
     let delete_film = move |filmId| {
@@ -47,7 +67,9 @@ fn App(cx: Scope) -> Element {
                         log::info!("Film deleted");
                         force_get_films.set(());
                     }
-                    Err(err) => log::info!("Error deleting film: {:?}", err),
+                    Err(err) => {
+                        log::info!("Error deleting film: {:?}", err);
+                    }
                 }
             }
         });
@@ -57,6 +79,7 @@ fn App(cx: Scope) -> Element {
         let force_get_films = force_get_films.clone();
         let current_selected_film = selected_film.clone();
         let is_modal_visible = is_modal_visible.clone();
+
         cx.spawn({
             async move {
                 let response = if current_selected_film.get().is_some() {
@@ -79,7 +102,9 @@ fn App(cx: Scope) -> Element {
                         is_modal_visible.write().0 = false;
                         force_get_films.set(());
                     }
-                    Err(err) => log::info!("Error creating film: {:?}", err),
+                    Err(err) => {
+                        log::info!("Error creating film: {:?}", err);
+                    }
                 }
             }
         });
@@ -127,23 +152,4 @@ fn App(cx: Scope) -> Element {
             }
         }
     })
-}
-
-fn films_endpoint() -> String {
-    let window = web_sys::window().expect("no global `window` exists");
-    let location = window.location();
-    let host = location.host().expect("should have a host");
-    let protocol = location.protocol().expect("should have a protocol");
-    let endpoint = format!("{}//{}/{}", protocol, host, API_ENDPOINT);
-    format!("{}/films", endpoint)
-}
-
-async fn get_films() -> Vec<Film> {
-    log::info!("Fetching films from {}", films_endpoint());
-    reqwest::get(&films_endpoint())
-        .await
-        .unwrap()
-        .json::<Vec<Film>>()
-        .await
-        .unwrap()
 }
